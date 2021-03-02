faf8b7387bb61ad9096dbc4d00c5701e2ee16d57
Morten Broesby-Olsen <mortenbroesby@gmail.com>
2021-03-02 16:22:01 +0100

    cleanup(project): Cleanup rollup config according to lint rules

--- rollup.config.js
+++ rollup.config.js
@@ -1,28 +1,31 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-var-requires */
+
 "use strict";
 
-import clear from 'rollup-plugin-clear';
-import replace from 'rollup-plugin-replace';
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import typescript from 'rollup-plugin-typescript2';
-import screeps from 'rollup-plugin-screeps';
+import clear from "rollup-plugin-clear";
+import replace from "rollup-plugin-replace";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "rollup-plugin-typescript2";
+import screeps from "rollup-plugin-screeps";
 
-const isProduction = process.env.NODE_ENV === 'production'
-const defaultConfigTarget = isProduction ? 'main' : 'sim';
+const isProduction = process.env.NODE_ENV === "production";
+const defaultConfigTarget = isProduction ? "main" : "sim";
 
 let configTarget = defaultConfigTarget;
 
 const destination = process.env.DEST;
 if (!destination) {
   console.log("No destination specified, using destination: ", configTarget);
-} else if ((require("./screeps.json")[destination]) == null) {
+} else if (require("./screeps.json")[destination] == null) {
   throw new Error("Invalid upload destination");
 } else {
   configTarget = destination;
   console.log("Using destination: ", configTarget);
 }
 
-const configFile = require("./screeps")[configTarget]
+const configFile = require("./screeps")[configTarget];
 
 export default {
   input: "src/main.ts",
@@ -39,7 +42,7 @@ export default {
     replace({
       PRODUCTION: JSON.stringify(isProduction),
       __BUILD_TIME__: JSON.stringify(Date.now()),
-      __REVISION__: JSON.stringify(require('git-rev-sync').short()),
+      __REVISION__: JSON.stringify(require("git-rev-sync").short())
     }),
 
     resolve({ rootDir: "src" }),
@@ -50,7 +53,7 @@ export default {
         // relative to the current directory, or the name
         // of a module in node_modules
 
-        'node_modules/my-lib/index.js': ['named']
+        "node_modules/my-lib/index.js": ["named"]
       }
     }),
 
@@ -60,7 +63,7 @@ export default {
       config: configFile,
 
       // if `NODE_ENV` is local, perform a dry run
-      dryRun: process.env.NODE_ENV === 'local'
+      dryRun: process.env.NODE_ENV === "local"
     })
   ]
-}
+};
